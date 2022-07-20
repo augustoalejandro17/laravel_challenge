1fdbdb2acc78270548393babd59e264f7dbfa211
Augusto Vaca <avaca@TEC6.romacc.com>
2022-07-20 16:37:45 -0500

    make migrations

--- database/migrations/2022_07_20_212114_create_people_table.php
+++ database/migrations/2022_07_20_212114_create_people_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersTable extends Migration
+class CreatePeopleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('people', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string("Dish");
-            $table->float("cost", 2);
-            $table->boolean("available");
+            $table->string("name");
+            $table->integer("age");
+            $table->boolean("isVip");
         });
     }
 
@@ -29,6 +29,6 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('people');
     }
 }
